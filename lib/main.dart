05b69c4a64a1bf976a670113c616b91991430cae
Bar Kobi <barkobi102@gmail.com>
2023-09-18 12:34:04 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:thaiapp/screeens/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Thai Demo',
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 11, 36, 71),
+        secondaryHeaderColor: const Color.fromARGB(255, 25, 55, 109),
+        highlightColor: const Color.fromARGB(255, 87, 108, 188),
+        hintColor: const Color.fromARGB(255, 165, 215, 232),
+        useMaterial3: true,
+      ),
+      home: const ThaiHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/screeens/home_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:thaiapp/widgets/chat_bubble.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class ThaiHomePage extends StatefulWidget {
+  const ThaiHomePage({super.key});
+
+  @override
+  State<ThaiHomePage> createState() => _ThaiHomePageState();
+}
+
+class _ThaiHomePageState extends State<ThaiHomePage> {
+  final TextEditingController _searchController = TextEditingController();
+
+  List<ChatBubble> chat = [
+    ChatBubble(
+        name: "Thai",
+        isMe: false,
+        message:
+            "Hi this the the Thai Bot here speaking, please enter any type of text you want to me to analayze, What is your name?")
+  ];
+
+  Future<void> addUser(String word) async {
+    const serverUrl = 'http://77.125.47.43:5000';
+    final response = await http.post(
+      Uri.parse('$serverUrl/send_message'),
+      body: json.encode({'username': word}),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      print('Response body: ${response.body}');
+      chat.add(ChatBubble(name: "Thai", message: response.body, isMe: false));
+      refreshChat();
+    } else {
+      print('Failed to add user. Status code: ${response.statusCode}');
+    }
+  }
+
+  void refreshChat() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
+
+    return Row(
+      children: [
+        Container(
+          width: deviceWidth * 0.7,
+          height: deviceHeight,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Scaffold(
+            backgroundColor: Theme.of(context).hintColor,
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: chat.length,
+                    itemBuilder: (context, index) => ChatBubble(
+                        name: chat[index].name,
+                        message: chat[index].message,
+                        isMe: chat[index].isMe),
+                  ),
+                ),
+                Container(
+                  width: deviceWidth,
+                  padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).padding.bottom),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          controller: _searchController,
+                          decoration: InputDecoration(
+                            hintText: 'Type a message...',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25)),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          String message = _searchController.text;
+                          chat.add(ChatBubble(
+                              name: "Bar", message: message, isMe: true));
+                          refreshChat();
+                          addUser(message);
+                        },
+                        child: const Icon(Icons.send_rounded),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          width: deviceWidth * 0.2,
+          color: Colors.black,
+          height: deviceHeight,
+          child: Scaffold(
+            appBar: AppBar(title: Text("Chats")),
+          ),
+        )
+      ],
+    );
+  }
+}
