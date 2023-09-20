cf06002dabb805d2cefc74050ccee41880dda7e2
ofek4454 <ofek4454@gmail.com>
2023-09-20 11:32:26 +0300

    add chat bubble and state

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,7 @@
+import './message.dart';
+
+class ChatModel {
+  List<Message> messages;
+
+  ChatModel() : messages = List<Message>.empty();
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:thaiapp/screens/home/local_widgets/chat.dart';
 import 'package:thaiapp/screens/home/local_widgets/drawer.dart';
 import 'package:thaiapp/screens/home_page.dart';
+import 'package:thaiapp/state_managment/chat_state.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Row(
         children: [
           ThaiDrawer(),
-          Chat(),
+          ChangeNotifierProvider<ChatState>(
+            create: (context) => ChatState(),
+            lazy: false,
+            child: Chat(),
+          ),
           //ThaiHomePage()
         ],
       ),
